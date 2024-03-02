081e1d534eddc1f38b2df91ce81560ef1e2a29a2
cnanjo@gmail.com <cnanjo@gmail.com>
2024-03-02 09:50:20 -0800

    Adding logging but still not working.

--- src/main/java/org/medontology/shex/sandbox/ShExValidator.java
+++ src/main/java/org/medontology/shex/sandbox/ShExValidator.java
@@ -18,7 +18,7 @@ public class ShExValidator {
     public static void main(String... args) {
 
         if(args.length != 4) {
-            System.out.println("Must provide four arguments: schema path, data path, focus node IRI, and shape IRI");
+            System.out.println("Must provide four arguments: [0] schema path, [1] data path, [2] focus node IRI, and [3] shape IRI");
             System.exit(0);
         } else {
             //http://shex.io/webapps/shex.js/doc/shex-simple.html?schema=BASE%20%3Chttp%3A%2F%2Fa.example%2F%3E%0A%0A%3CS%3E%20%20%7B%0A%20%3Cp%3E%20%5B0%5D%0A%7D%0A%0A%3CT%3E%20extends%20%40%3CS%3E%20%7B%0A%20%3Cp%3E%20%5B1%5D%0A%7D%0A%0A&data=BASE%20%3Chttp%3A%2F%2Fa.example%2F%3E%0A%0A%3Chttp%3A%2F%2Fa.example%2Fx%3E%20%3Cp%3E%200%2C%201%20.%0A&shape-map=%3Chttp%3A%2F%2Fa.example%2Fx%3E%40%3Chttp%3A%2F%2Fa.example%2FT%3E&interface=human&success=proof&regexpEngine=eval-threaded-nerr&manifestURL=http%3A%2F%2Fshex.io%2Fwebapps%2Fshex.js%2Fexamples%2Fmanifest.json
@@ -31,11 +31,14 @@ public class ShExValidator {
             //String SHAPES = "shex/schema/ExtendAND3G.shex";
             //String DATA = "shex/data/sABCDE.ttl";
 
-            System.out.println("Read data");
+            System.out.println("Reading data from: " + dataFilePath);
             Graph dataGraph = RDFDataMgr.loadGraph(dataFilePath);
 
-            System.out.println("Read shapes");
+            System.out.println("Loading schema: " + schemaPath);
             ShexSchema shexSchema = Shex.readSchema(schemaPath);
+
+            System.out.println("Using focus node: " + focusNodeIri);
+            System.out.println("Using shape: " + shapeIri);
 
             // Shapes map.
             Node focusNode = NodeFactory.createURI(focusNodeIri);
